752cf7cb42b46c2ac7879c507fb836d909bbbedd
dinuka <dinukathilakarathne@gmail.com>
2024-05-01 17:08:21 +0530

    flow changes

---
+++ .codedworkflows/ConnectionsManager.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+using System;
+
+namespace REFrameworkCommonTemplatev0._03
+{
+    public class ConnectionsManager
+    {
+        public ConnectionsManager(ICodedWorkflowsServiceContainer resolver)
+        {
+        }
+    }
+}
