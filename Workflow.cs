52dcfaca53c29e0b26db281971fd25f7c820ba8d
dinuka <dinukathilakarathne@gmail.com>
2025-08-19 19:31:22 +0530

    code base Uipath development emails modifications add onto the extra workflow

--- Workflow.cs
+++ Workflow.cs
@@ -1,3 +1,5 @@
+using Microsoft.Graph;
+using UiPath.Core.Activities;
 using REFrameworkCommonTemplatev0._03.ObjectRepository;
 using System;
 using System.Collections.Generic;
@@ -18,6 +20,8 @@ using UiPath.Testing.Enums;
 using UiPath.UIAutomationNext.API.Contracts;
 using UiPath.UIAutomationNext.API.Models;
 using UiPath.UIAutomationNext.Enums;
+using System.Net;
+using System.Net.Mail;
 
 namespace REFrameworkCommonTemplatev0._03
 {
@@ -26,12 +30,37 @@ namespace REFrameworkCommonTemplatev0._03
         [Workflow]
         public void Execute()
         {
-            // To start using services, use IntelliSense (CTRL + Space) to discover the available services:
-            // e.g. system.GetAsset(...)
 
-            // For accessing UI Elements from Object Repository, you can use the Descriptors class e.g:
-            // var screen = uiAutomation.Open(Descriptors.MyApp.FirstScreen);
-            // screen.Click(Descriptors.MyApp.FirstScreen.SettingsButton);
+
+// Input parameters (you can pass them from UiPath arguments/variables)
+string fromAddress = "dthilakarathne@innobothealth.com";
+string toAddress = "dthilakarathne@innobothealth.com";
+string subject = "Hey Sexy What's up";
+string body = "Hello! This is Your WTF Bot.";
+
+// SMTP Configuration
+string smtpHost = "smtp.office365.com";   // Example: Gmail: smtp.gmail.com, Outlook: smtp.office365.com
+int smtpPort = 587;
+string username = "dthilarathne@innobothealth.com";
+string password = "!@#09235";  //
+
+// Create and send the message
+var smtp = new SmtpClient(smtpHost, smtpPort)
+{
+    Credentials = new NetworkCredential(username, password),
+    EnableSsl = true
+    PasswordCredential = true
+        New password = new NetworkConnection (username, password)
+
+};
+
+MailMessage message = new MailMessage(fromAddress, toAddress, subject, body);
+smtp.Send(message);
+
+            
+            
+            List<int> inputNumbers = new List<int>() { 40, 50, 100, 80, 60, 90 }; // This would typically be an 'In' argument
+int outLargestNumber; // This would be an 'Out' argument
         }
     }
 }
