d763e7f967b3717be87f39992625480528fd7d3f
dinuka <dinukathilakarathne@gmail.com>
2025-08-20 17:51:51 +0530

    updated

--- Workflow.cs
+++ Workflow.cs
@@ -1,3 +1,4 @@
+using UiPath.Mail;
 using Microsoft.Graph;
 using UiPath.Core.Activities;
 using REFrameworkCommonTemplatev0._03.ObjectRepository;
@@ -22,6 +23,8 @@ using UiPath.UIAutomationNext.API.Models;
 using UiPath.UIAutomationNext.Enums;
 using System.Net;
 using System.Net.Mail;
+using Tesseract; 
+using System.Drawing; // For image processing if needed
 
 namespace REFrameworkCommonTemplatev0._03
 {
@@ -50,17 +53,53 @@ var smtp = new SmtpClient(smtpHost, smtpPort)
     Credentials = new NetworkCredential(username, password),
     EnableSsl = true
     PasswordCredential = true
-        New password = new NetworkConnection (username, password)
+    New password = new NetworkConnection (username, password)
+    
 
 };
 
 MailMessage message = new MailMessage(fromAddress, toAddress, subject, body);
 smtp.Send(message);
+    String subject = "2nd Try Email . . . . "
+MailMessageExtensions new Message = new MailAddressCollection (from, toAdd, subj, body);
 
-            
-            
+
             List<int> inputNumbers = new List<int>() { 40, 50, 100, 80, 60, 90 }; // This would typically be an 'In' argument
-int outLargestNumber; // This would be an 'Out' argument
+
         }
+    }
+}
+
+public class PdfOcrProcessor
+{
+    public string ExtractTextFromPdf(string pdfFilePath)
+    {
+        string extractedText = "";
+        try
+        {
+            // Convert PDF pages to images (requires a PDF rendering library like PdfiumViewer or similar)
+            // For simplicity, let's assume you have an image file already
+            using (var engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default))
+            {
+                using (var img = Pix.LoadFromFile("engine")) 
+                {
+                    using (var page = engine.Process(img))
+                    {
+                        extractedText = page.GetText();
+                        Console.WriteLine("OCR Try Block $: {ex.Message}");
+                        extractedText = page.GetText(0);
+                        Console.WriteLine(page,"$: Console Page passed");
+                        
+                    }
+                }
+            }
+        }
+        catch (TesseractException ex)
+        {
+            Console.WriteLine($"Error during OCR: {ex.Message}");
+            Console.WriteLine("OCR Exception OCR: {ex.Message}");
+            
+        }
+        return extractedText;
     }
 }
